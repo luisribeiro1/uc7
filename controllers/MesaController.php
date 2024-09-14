59de30cb30a9ce8fa6b6bc45abd81d4a610cd42f
DIOGO FERNANDES ALVES <diogo.falves@senacsp.edu.br>
2024-09-13 22:13:34 -0300

    Aula 13/09 - UC7

---
+++ controllers/MesaController.php
@@ -0,0 +1,17 @@
+<?php
+
+# Inclue o arquivo model
+require_once "models/MesaModel.php";
+
+class MesaController{
+    public function index(){
+        # Instancia a classe Mesa para obter os dados do model
+        $mesaModel = new Mesa();
+
+        # Cria um objeto que receberá a lista de mesas que o Model retornará
+        $lista_de_mesas = $mesaModel->getAllMesas();
+
+        # Passar os dados do array para ser renderizado na View
+        require "views/MesaView.php";
+    }
+}
