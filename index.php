2515c6a2893aa44eb95ac92bfca18d73aeca484d
LETICIA MARIA SILVA RAMOS DE OLIVEIRA <leticia.msroliveira@senacsp.edu.br>
2024-09-13 22:13:09 -0300

    Aula 13/09/2024: UC7|Restaurante-MVC|Listagem de Mesas

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+# Captar a URL redirecionada no .htaccess ($_ indica uma super global).
+# trim limpa caracteres vazios no início e final de texto.
+# strtolower() converte para
+$requisicao = trim(strtolower($_SERVER['REQUEST_URI']));
+
+# Substituir a parte da URL que não é útil.
+$requisicao = str_replace("/uc7/restaurante-mvc/","",$requisicao);
+
+# Divide em partes, usando a barra como separador. Cria um array de índice
+$segmentos = explode("/",$requisicao);
+
+# Verifica o padrão da rota
+$controlador = isset($segmentos[0]) ? $segmentos[0] : null;
+$metodo = isset($segmentos[1]) ? $segmentos[1] : null;
+$identificador = isset($segmentos[2]) ? $segmentos[2] : null;
+
+
+
+# mesa/editar/4
+# controller        (mesa)
+# método            (editar)
+# identificador     (4)
+
+switch($controlador){
+    case "mesa":
+        require "controllers/MesaController.php";
+        $controller = new MesaController();
+        $controller->index();
+        break;
+    default:
+        echo "Página não encontrada";
+        break;
+}
+
+
