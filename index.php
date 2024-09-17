83f91b5ea1c172d312dec562afb5127744435a83
GUILHERME HENRIQUE SAMPAIO <guilherme.hsampaio@senacsp.edu.br>
2024-09-17 19:37:37 -0300

    guilherme 17/09/2024

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+# Captar a URL redirecionada no .htaccess ($_ indica uma super global)
+# trim() limpa caracteres vazios no inicio e final do texto.
+# strlower() converte tudo o que for digitado na URL em letras minusculas
+$requisicao = trim(strtolower($_SERVER['REQUEST_URI']));
+
+# Substituir a parte da URL que não é útil
+$requisicao = str_replace("/uc7/restaurante-mvc/","", $requisicao);
+
+# Divide em partes, usando a barra como separador. Cria um array de índice 
+$segmentos = explode("/",$requisicao);
+
+# Verifica o padrão da rota
+$controlador = isset($segmentos[0]) ? $segmentos[0] : null;
+$metodo = isset($segmentos[1]) ? $segmentos[1] : null;
+$identificador = isset($segmentos[2]) ? $segmentos[2] : null;
+
+# mesa/editar/4
+# controller        (mesa)
+# método            (editar)
+# identificador     (4)
+
+switch($controlador){
+    case "mesa":
+        require "controllers/MesaController.php";
+        $controller = new MesaController();
+        $controller->index();
+        break;
+
+    default:
+        echo "Página não encontrada";
+        break;
+}
