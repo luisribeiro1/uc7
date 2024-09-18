ab6f1f98a18f74b449a618d5a9c66bb9d8d231fb
ANDERSON ANDRE PARDINHO DA SILVA <anderson.apsilva2@senacsp.edu.br>
2024-09-17 22:17:22 -0300

    Aula dia 17/09/2024

---
+++ Restaurante-MVC/models/DataBase.php
@@ -0,0 +1,38 @@
+<?php
+# Estabelecer a conexão com o banco de Dados
+
+class DataBase{
+    # Atributo privado e estatico
+    private static $conexao = null;
+
+    # metodo publico e estatico
+    public static function getConexao(){
+           
+
+        if(self::$conexao == null){
+            $host = "localhost";
+            $nomeDoBanco = "restaurante-mvc";
+            $usuario = "root";
+            $senha = "";
+
+            try{
+
+                self::$conexao = new PDO(
+                    "mysql:host=$host;dbname=$nomeDoBanco",
+                    $usuario,
+                    $senha
+                );
+                self::$conexao->setAttribute(
+                    PDO::ATTR_ERRMODE, 
+                    PDO::ERRMODE_EXCEPTION
+                );
+           
+             
+
+            }  catch(PDOException $erro){
+                echo "Erro de conexao: " . $erro->getMessage();
+            }
+        }
+        return self::$conexao;
+    } 
+}
--- Restaurante-MVC/models/MesaModel.php
+++ Restaurante-MVC/models/MesaModel.php
@@ -1,22 +1,33 @@
 <?php
+require_once "DataBase.php";
 
 class Mesa 
 {
 
  #criar um array associativa com a relaçao das mesas
-    private $listaDeMesas = [
-        ["id" => 1, "lugares" => 4, "tipo" => "quadrada"],
-        ["id" => 2, "lugares" => 6, "tipo" => "oval"],
-        ["id" => 3, "lugares" => 4, "tipo" => "quadrada"],
-        ["id" => 4, "lugares" => 8, "tipo" => "retangular"],
-        ["id" => 5, "lugares" => 2, "tipo" => "redonda"],
-        ["id" => 6, "lugares" => 6, "tipo" => "redonda"],
-        ["id" => 7, "lugares" => 8, "tipo" => "quadrada"],
-        ["id" => 8, "lugares" => 2, "tipo" => "redonda"],
+    // private $listaDeMesas = [
+    //     ["id" => 1, "lugares" => 4, "tipo" => "quadrada"],
+    //     ["id" => 2, "lugares" => 6, "tipo" => "oval"],
+    //     ["id" => 3, "lugares" => 4, "tipo" => "quadrada"],
+    //     ["id" => 4, "lugares" => 8, "tipo" => "retangular"],
+    //     ["id" => 5, "lugares" => 2, "tipo" => "redonda"],
+    //     ["id" => 6, "lugares" => 6, "tipo" => "redonda"],
+    //     ["id" => 7, "lugares" => 8, "tipo" => "quadrada"],
+    //     ["id" => 8, "lugares" => 2, "tipo" => "redonda"],
         
-    ];
+    // ];
+
+    # Criar um atributo privado para receber a conexao com o banco
+    private $db;
+    # Metodo construtor da classe
+    public function __construct(){
+        $this->db = DataBase::getConexao();
+    }
+
     #criar o metodo para retornar a lista de mesas
     public function getAllMesas(){
-        return $this->listaDeMesas;
+       // return $this->listaDeMesas;
+       $sql = $this->db->query("SELECT * FROM mesas");
+       return $sql->fetchAll(PDO::FETCH_ASSOC);
     }
 }
