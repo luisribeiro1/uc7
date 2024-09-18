b71ae3419cc283d497ac340f2fd520501e0d4181
MURILO ALMEIDA DE OLIVEIRA <murilo.aoliveira11@senacsp.edu.br>
2024-09-17 22:16:12 -0300

    17/09/2024 Criação e criação banco de dados Restaurante MVC

--- models/MesaModel.php
+++ models/MesaModel.php
@@ -1,24 +1,35 @@
 <?php
 
+
+require_once "DataBase.php";
 class Mesa
 {
     # Criar um array associativo com a relação das mesas 
-    private $listaDeMesas = [
-        ["id" => 1, "lugares" => 4, "tipo" => "quadrada"],
-        ["id" => 2, "lugares" => 2, "tipo" => "oval"],
-        ["id" => 3, "lugares" => 6, "tipo" => "redonda"],
-        ["id" => 4, "lugares" => 8, "tipo" => "retangular"],
-        ["id" => 5, "lugares" => 4, "tipo" => "redonda"],
-        ["id" => 6, "lugares" => 10, "tipo" => "quadrada"],
-        ["id" => 7, "lugares" => 4, "tipo" => "oval"],
-        ["id" => 8, "lugares" => 6, "tipo" => "redonda"],
-        
-    ];
+    // private $listaDeMesas = [
+    //     ["id" => 1, "lugares" => 4, "tipo" => "quadrada"],
+    //     ["id" => 2, "lugares" => 2, "tipo" => "oval"],
+    //     ["id" => 3, "lugares" => 6, "tipo" => "redonda"],
+    //     ["id" => 4, "lugares" => 8, "tipo" => "retangular"],
+    //     ["id" => 5, "lugares" => 4, "tipo" => "redonda"],
+    //     ["id" => 6, "lugares" => 10, "tipo" => "quadrada"],
+    //     ["id" => 7, "lugares" => 4, "tipo" => "oval"],
+    //     ["id" => 8, "lugares" => 6, "tipo" => "redonda"]
+    // ];
+
+    # Criar um atributo privado para receber a conexão com o banco
+    private $db;
+
+    # Método construtor da classe
+    public function __construct(){
+        $this->db = DataBase::getConexao();
+    }
 
     # Criar o método para retornar a lista de mesas 
     public function getAllMesas(){
-        return $this->listaDeMesas;
-    } 
+        // return $this->listaDeMesas;
+        $sql = $this->db->query("SELECT * FROM mesas");
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
 
     
 }
