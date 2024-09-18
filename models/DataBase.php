fdee2447459ce8d1cb353c80dee2b2e81224e720
JOSE HENRIQUE DE BRITO DOS SANTOS <jose.hbsantos@senacsp.edu.br>
2024-09-17 22:15:05 -0300

    Aula do dia 17/09/2024

---
+++ models/DataBase.php
@@ -0,0 +1,35 @@
+<?php
+
+# Estabelecer a conexão com o banco de dados
+
+class DataBase{
+    # Atributo privado de estático
+    private static $conexao = null;
+
+    # Método público estático
+    public static function getConexao(){
+        if (self::$conexao == null){
+            $host = "localhost";
+            $nomeDoBanco = "restaurante-mvc";
+            $usuario = "root";
+            $senha = "";
+
+            try{
+                
+                self::$conexao = new PDO(
+                    "mysql:host=$host;dbname=$nomeDoBanco",
+                    $usuario,
+                    $senha
+                );
+                self::$conexao->setAttribute(
+                    PDO::ATTR_ERRMODE,
+                    PDO::ERRMODE_EXCEPTION
+                );
+
+            } catch (PDOException $erro){
+                echo "Erro de conexão: " . $erro->getMessage();
+            }
+        }
+        return self::$conexao;
+    }
+}
