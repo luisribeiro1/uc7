0ebdedaad38c0116a59d822902e1841a1da878cd
GLAUBER BASSO RUIZ <glauber.bruiz@senacsp.edu.br>
2024-09-17 22:16:01 -0300

    Aula do dia 17/09/2024

--- models/MesaModel.php
+++ models/MesaModel.php
@@ -1,22 +1,35 @@
 <?php
 
+require_once "DataBase.php";
+
 class Mesa
 {
     # Criar um array associativo com a relação das mesas
-    private $listaDeMesas = [
-        ['id' => 1, 'lugares' => 4, 'tipo' => 'quadrada'],
-        ['id' => 2, 'lugares' => 6, 'tipo' => 'oval'],
-        ['id' => 3, 'lugares' => 4, 'tipo' => 'quadrada'],
-        ['id' => 4, 'lugares' => 8, 'tipo' => 'retangular'],
-        ['id' => 5, 'lugares' => 2, 'tipo' => 'redonda'],
-        ['id' => 6, 'lugares' => 6, 'tipo' => 'oval'],
-        ['id' => 7, 'lugares' => 8, 'tipo' => 'quadrada'],
-        ['id' => 8, 'lugares' => 2, 'tipo' => 'redonda'],
-    ];
+    //  private $listaDeMesas = [
+    //      ["id" => 1, "lugares" => 4, "tipo" => "quadrada"],
+    //      ["id" => 2, "lugares" => 6, "tipo" => "oval"],
+    //      ["id" => 3, "lugares" => 4, "tipo" => "quadrada"],
+    //      ["id" => 4, "lugares" => 8, "tipo" => "retangular"],
+    //      ["id" => 5, "lugares" => 2, "tipo" => "redonda"],
+    //      ["id" => 6, "lugares" => 6, "tipo" => "redonda"],
+    //      ["id" => 7, "lugares" => 8, "tipo" => "quadrada"],
+    //      ["id" => 8, "lugares" => 10, "tipo" => "redonda"],
+    //  ];
 
-    # Criar o método para retornar a lista de mesas
+    # Criar um atributo privado para receber a conexão com o banco 
+    private $db;
+
+    # Método construtor da classe
+    public function __construct(){
+        $this->db = DataBase::getConexao();
+    }
+    
+    # Criar o método para retornar a lista de meses
     public function getAllMesas(){
-        return $this->listaDeMesas;
+        // return $this->listaDeMesas;
+        
+        $sql = $this->db->query("SELECT * FROM mesas");
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
