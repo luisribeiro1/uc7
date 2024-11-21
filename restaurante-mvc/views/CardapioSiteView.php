16fde4aa9eecb1a92eb52ba9f20b934cc5b58f7a
Luis Henrique <luis.h.souza@outlook.com.br>
2024-11-20 21:36:18 -0300

    feat: ContatoView.php

--- restaurante-mvc/views/CardapioSiteView.php
+++ restaurante-mvc/views/CardapioSiteView.php
@@ -39,7 +39,7 @@ $html = file_get_contents("views/templates/html/modeloSite.html");
 
 # substituir a tag [[header]] pelo conteúdo da variável $header. O mesmo acontece com as demais.
 $html = str_replace("[[header]]", $header, $html);
-$html = str_replace("[[titulo]]", "CARDÁPIO", $html);
+$html = str_replace("[[titulo]]", "<span><svg xmlns='http://www.w3.org/2000/svg' width='36' height='36' viewBox='0 0 24 24' style='fill: rgba(50, 93, 136, 1);transform: ;msFilter:;'><path d='M3 2h2v20H3zm16 0H6v20h13c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm-1 10H9v-2h9v2zm0-4H9V6h9v2z'></path></svg></span><span class='ms-2 fw-bold'>CARDÁPIO</span>", $html);
 $html = str_replace("[[conteudo]]", $lista, $html);
 $html = str_replace("[[footer]]", $footer, $html);
 $html = str_replace("[[base-url]]", $baseUrl, $html);
--- restaurante-mvc/views/ContatoView.php
+++ restaurante-mvc/views/ContatoView.php
@@ -9,6 +9,7 @@ $lista = "";
 
 # iterar sobre o array $lista_de_pizzas que contém as informações das pizzas
 foreach ($lista_de_contatos as $contato) {
+
   $rua = $contato['rua'];
   $bairro = $contato['bairro'];
   $cidade = $contato['cidade'];
@@ -18,45 +19,65 @@ foreach ($lista_de_contatos as $contato) {
   # obter os itens das redes sociais que estão em um array
   $lista_de_redes = "";
   $resultado_rede = "";
+  $rede = "";
+  
+  $localizacao = "";
+  
+  if ($rua == 'Av Paulista, 2389') {
+    $localizacao = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.323247957456!2d-46.66495532478867!3d-23.55683126142554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce582d158d348d%3A0x9860ea40cc0b82ff!2sAv.%20Paulista%2C%202389%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001311-300!5e0!3m2!1spt-BR!2sbr!4v1732140733850!5m2!1spt-BR!2sbr';
+
+  } elseif ($rua == 'Rua Quadros Sobrinho, 01') {
+    $localizacao = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.317702257828!2d-47.401501024823816!3d-22.567217925823332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c880ee0caac24d%3A0x95fcf7d5914d4d50!2sSenac%20Limeira!5e0!3m2!1spt-BR!2sbr!4v1732145782348!5m2!1spt-BR!2sbr';
+
+  } elseif ($rua == 'Avenida do Café, 298') {
+    $localizacao = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.160034128717!2d-46.64200742478574!3d-23.634439164280845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5afae092693d%3A0xf823896922271ef9!2sSenac%20Jabaquara!5e0!3m2!1spt-BR!2sbr!4v1732145862256!5m2!1spt-BR!2sbr';
+
+  } elseif ($rua == 'Av. Frei Orestes Girardi, 3549') {
+    $localizacao = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3680.1857172157706!2d-45.579846424818506!3d-22.721337531269366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc884b6b6c0513%3A0x27b5e18e02518691!2sCentro%20Universit%C3%A1rio%20Senac%20-%20Campos%20do%20Jord%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1732146044885!5m2!1spt-BR!2sbr';
+
+  } elseif ($rua == 'Rua Sacramento, 490') {
+    $localizacao = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.385633529476!2d-47.06379632481216!3d-22.899142837597733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c8ad9bce3363%3A0x7470022cd79a39c4!2sSenac%20Campinas!5e0!3m2!1spt-BR!2sbr!4v1732146445502!5m2!1spt-BR!2sbr';
+  }
+
+  $rede_facebook = "<i class='bi bi-facebook fs-5'></i>";
+  $rede_instagram = "<i class='bi bi-instagram fs-5'></i>";
+  $rede_linkedin = "<i class='bi bi-linkedin'></i>";
+  $rede_tiktok = "<i class='bi bi-tiktok'></i>";
+  $rede_whatsapp = "<i class='bi bi-whatsapp'></i>";
+  $rede_youtube = "<i class='bi bi-youtube'></i>";
 
   foreach ($contato['contatos'] as $rede) {
 
     $rede_null = 'd-none';
-# oque / troca / onde
+
     switch ($resultado_rede) {
 
       case strpos($rede, 'facebook'):
-        $rede = "<i class='bi bi-facebook fs-5'></i>";
-        $lista_de_redes .= "<a class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</a>";
+        $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</span>";
         break;
 
       case strpos($rede, 'instagram'):
-        $rede = "<i class='bi bi-instagram fs-5'></i>";
-        $lista_de_redes .= "<a class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</a>";
+        $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</span>";
         break;
 
-      case strpos($rede, 'youtube'):
-        $rede = "<i class='bi bi-youtube'></i>";
-        $lista_de_redes .= "<a class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</a>";
+        case $rede_youtube = strpos($rede, 'youtube'):
+        $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</span>";
         break;
 
       case strpos($rede, 'tiktok'):
-        $rede = "<i class='bi bi-tiktok'></i>";
-        $lista_de_redes .= "<a class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</a>";
+        $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</span>";
         break;
 
       case strpos($rede, 'linkedin'):
-        $rede = "<i class='bi bi-linkedin'></i>";
-        $lista_de_redes .= "<a class='btn btn-sm btn-secondary rounded-pill my-1 me-1' href='$rede'></a>";
+        $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</span>";
         break;
 
       case strpos($rede, 'whatsapp'):
-        $rede = "<i class='bi bi-whatsapp'></i>";
-        $lista_de_redes .= "<a class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</a>";
+        $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1'>$rede</span>";
         break;
       
       default:
-        $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1 $rede_null'></span>";
+        $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill $rede_null'></span>";
         break;
     }
 
@@ -65,28 +86,29 @@ foreach ($lista_de_contatos as $contato) {
     // } else {
     //   $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1 $rede_null'></span>";
     // };
-
     // $lista_de_redes .= "<span class='btn btn-sm btn-secondary rounded-pill my-1 me-1 $rede_null'>$rede</span>";
   }
 
   # criar a estrutura HTML
   $lista .="
     <div class='col-md-12 mb-4'>
-      <div class='card shadow rounded-4'>
-        <div class='card-header'>
-          <h5>
-            $rua - $bairro - $cidade
-          </h5>
+      <div class='card shadow rounded-3'>
+        <div class='card-header header d-flex flex-column'>
+          <h4 class='mt-2 text-white fw-semibold'>Senac</h5>
+          <p class='text-white'><span class='fw-medium'>Endereço:</span> $rua - $bairro - $cidade</p>
+          <p class='text-white'><span class='fw-medium'>Horário de funcionamento:</span> $horario</p>
         </div>
 
         <div class='card-body'>
-          <p>$horario</p></br>
-          $lista_de_redes </br>
+          <iframe src=$localizacao width='100%' height='450' style='border:0' allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'>
+        </iframe>
+        </div>
 
+        <div class='card-footer'>
+          $lista_de_redes
         </div>
       </div>
     </div>
-
   ";
 }
 
@@ -97,7 +119,7 @@ $html = file_get_contents("views/templates/html/modeloSite.html");
 
 # substituir a tag [[header]] pelo conteúdo da variável $header. O mesmo acontece com as demais.
 $html = str_replace("[[header]]", $header, $html);
-$html = str_replace("[[titulo]]", "<span class=''><span><span><strong class='ms-2'>CONTATOS</strong></span>", $html);
+$html = str_replace("[[titulo]]", "<span class=''><svg xmlns='http://www.w3.org/2000/svg' width='36' height='36' viewBox='0 0 24 24' style='fill: rgba(50, 93, 136, 1);transform: ;msFilter:;'><path d='M21 2H6a2 2 0 0 0-2 2v3H2v2h2v2H2v2h2v2H2v2h2v3a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zm-8 2.999c1.648 0 3 1.351 3 3A3.012 3.012 0 0 1 13 11c-1.647 0-3-1.353-3-3.001 0-1.649 1.353-3 3-3zM19 18H7v-.75c0-2.219 2.705-4.5 6-4.5s6 2.281 6 4.5V18z'></path></svg><span><span><strong class='ms-2'>CONTATOS</strong></span>", $html);
 $html = str_replace("[[conteudo]]", $lista, $html);
 $html = str_replace("[[footer]]", $footer, $html);
 $html = str_replace("[[base-url]]", $baseUrl, $html);
