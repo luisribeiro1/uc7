a498f5dab540c0c9d04c72e4c02f594ec71b4977
DIOGO FERNANDES ALVES <diogo.falves@senacsp.edu.br>
2024-11-21 21:57:48 -0300

    Contatos - 21/11/2024

--- views/ContatoView.php
+++ views/ContatoView.php
@@ -1,7 +1,7 @@
 <?php
 
 # Variavel para incluir os códigos HTML da página
-$lista = "";
+$lista = "";    
 
 foreach($contatos as $enderecos){
     $rua = $enderecos["rua"];
@@ -11,15 +11,39 @@ foreach($contatos as $enderecos){
     $contatos = $enderecos["contatos"];
     $unidade = $enderecos["unidade"];
 
+    $lista_de_contatos = "";
+    $link = [
+    'facebook' => "<i class='bi bi-facebook'></i> Facebook",
+    'instagram' => "<i class='bi bi-instagram'></i> Instagram",
+    'youtube' => "<i class='bi bi-youtube'></i> YouTube",
+    'tiktok' => "<i class='bi bi-tiktok'></i> TikTok",
+    'linkedin' => "<i class='bi bi-linkedin'></i> Linkedin",
+    'whatsapp' => "<i class='bi bi-whatsapp'></i> WhatsApp",
+        ];
     
+    foreach($contatos as $contato){
+        foreach($link as $social => $valor){
+            
+            if(strpos($contato, $social)){
+                $lista_de_contatos.="
+                
+                    <a href='$contato' target='_blank' class='btn btn-sm btn-secondary'>
+                    $valor
+                    </a>
+                    ";
+            }
+        }
+    }
+
     $lista.="
         <div class='col-md-12 mb-4'>
 
             <div class='card shadow'>
             <span class='d-block p-2 bg-primary text-light text-bold'>$rua-$bairro-$cidade</span>
                 <div class='card-body'>
-            $horario
-
+                <p class='mt-4 mx-2 text-secondary-emphasis'>$horario</p> 
+                <div class='container col-md-7 mb-4 mx-2'>$lista_de_contatos</div>
+                
         </div>
     </div>
 </div>
